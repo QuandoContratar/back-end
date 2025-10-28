6edc109010819d5a1de960b21d0e1f59f980b650
Isabel <isabel.oliveira@sptech.school>
2025-10-28 00:56:57 -0300

    subindo candidateMatch

---
+++ api/src/main/kotlin/project/api/app/match/CandidateMatch.kt
@@ -0,0 +1,35 @@
+package project.api.app.match
+
+import jakarta.persistence.*
+import project.api.app.candidates.data.Candidate
+import project.api.app.vacancies.data.Vacancy
+
+@Entity
+@Table(name = "candidate_match")
+data class CandidateMatch(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int? = null,
+
+    @ManyToOne
+    @JoinColumn(name = "candidate_id", nullable = false)
+    val candidate: Candidate,
+
+    @ManyToOne
+    @JoinColumn(name = "vacancy_id", nullable = false)
+    val vacancy: Vacancy,
+
+    @Column(nullable = false)
+    val score: Double,
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    val matchLevel: MatchLevel
+)
+
+enum class MatchLevel {
+    BAIXO,
+    MEDIO,
+    ALTO,
+    DESTAQUE
+}
--- api/src/main/kotlin/project/api/app/match/CandidateMatchRepository.kt
+++ api/src/main/kotlin/project/api/app/match/CandidateMatchRepository.kt
@@ -7,4 +7,10 @@ interface CandidateMatchRepository : JpaRepository<CandidateMatch, Int> {
     fun findByVacancyId(vacancyId: Int): List<CandidateMatch>
     fun findByCandidate_idCandidate(candidateId: Int): List<CandidateMatch>
 
+//    @Query("SELECT m FROM CandidateMatch m WHERE m.vacancy.id_vacancy = :vacancyId")
+//    fun findByVacancyId(@Param("vacancyId") vacancyId: Int): List<CandidateMatch>
+//
+//    @Query("SELECT m FROM CandidateMatch m WHERE m.candidate.id_candidate = :candidateId")
+//    fun findByCandidateId(@Param("idCandidate") candidateId: Int): List<CandidateMatch>
+
 }
