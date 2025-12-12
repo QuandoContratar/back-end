28b30d780931556fe9e3e9b1cc69a30763937edb
F3Alexandre <amurata@f3capital.com.br>
2025-12-12 16:15:05 -0300

    fix

---
+++ inserts2.sql
@@ -0,0 +1,7 @@
+INSERT INTO opening_requests (cargo, periodo, modelo_trabalho, regime_contratacao, salario, localidade, requisitos, justificativa_path, gestor_id, area, status)
+    VALUES
+    ('Desenvolvedor Frontend', 'Diurno', 'remoto', 'CLT', 4200, 'São Paulo', 'React, JS', NULL, 2, 'TI', 'APROVADA'),
+    ('Analista Financeiro', 'Diurno', 'presencial', 'CLT', 5000, 'São Paulo', 'Excel, ERP', NULL, 4, 'Financeiro', 'APROVADA'),
+    ('UX Researcher', 'Integral', 'híbrido', 'CLT', 4800, 'Rio de Janeiro', 'Figma, Pesquisa UX', NULL, 3, 'Marketing', 'APROVADA'),
+    ('QA Analyst', 'Integral', 'remoto', 'CLT', 5200, 'São Paulo', 'Testes manuais', NULL, 2, 'TI', 'APROVADA'),
+    ('Estagiário Comercial', 'Diurno', 'presencial', 'CLT', 2000, 'São Paulo', 'Suporte vendas', NULL, 5, 'Comercial', 'APROVADA');
